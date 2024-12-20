e0a6c985758e70c99035ff0ad4fe9d23e120dfff
object814 <zhouhy0108@gmail.com>
2024-12-20 17:56:42 +0800

    use random initialization in evaluation

--- experiments/robot/libero/libero_utils.py
+++ experiments/robot/libero/libero_utils.py
@@ -21,7 +21,8 @@ def get_libero_env(task, model_family, resolution=256):
     task_bddl_file = os.path.join(get_libero_path("bddl_files"), task.problem_folder, task.bddl_file)
     env_args = {"bddl_file_name": task_bddl_file, "camera_heights": resolution, "camera_widths": resolution}
     env = OffScreenRenderEnv(**env_args)
-    env.seed(0)  # IMPORTANT: seed seems to affect object positions even when using fixed initial state
+    # env.seed(0)  # IMPORTANT: seed seems to affect object positions even when using fixed initial state
+    env.seed(np.random.randint(0, 100)) # We set random seed for evaluation
     return env, task_description
 
 
